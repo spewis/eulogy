583deabaed7efc33d08f0bc152b093544df990f0
lewis <lewisattey@gmail.com>
2022-04-18 13:06:22 +1000

    Removed proxy server

--- client/main.js
+++ client/main.js
@@ -99,10 +99,8 @@ function handleName(data) {
 
 // FACE REQUEST — SPECIFY GENDER //
 function randomGender() {
-  let maleUrl =
-    "https://thingproxy.freeboard.io/fetch/https://fakeface.rest/face/json?gender=male";
-  let femaleUrl =
-    "https://thingproxy.freeboard.io/fetch/https://fakeface.rest/face/json?gender=female";
+  let maleUrl = "https://fakeface.rest/face/json?gender=male";
+  let femaleUrl = "https://fakeface.rest/face/json?gender=female";
   let prob = Math.random(0.5);
   if (prob < 0.5) {
     result = maleUrl;
